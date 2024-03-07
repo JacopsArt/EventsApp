1bb2b17caa724883fa7e056ce5279d11ee1c1e3d
Jermaine <jermaine.aroma@gmail.com>
2024-03-07 22:21:35 +0100

    fixed responsivenes of search- and category input

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -3,17 +3,13 @@ import { Input, Box } from "@chakra-ui/react";
 
 const CategoryFilter = ({ value, onChange, categories }) => {
   if (!categories) {
-    return null; 
+    return null;
   }
 
   return (
-    <Box mx="auto" width="10%">
-      <Input
-        placeholder="Category"
-        value={value}
-        onChange={onChange}
-        mb="4"
-      />
+    <Box mx="auto" width={{ base: "80%", md: "40%", lg: "20%" }}>
+      {" "}
+      <Input placeholder="Category" value={value} onChange={onChange} mb="4" />
     </Box>
   );
 };
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -3,7 +3,9 @@ import { Input, Box } from "@chakra-ui/react";
 
 const SearchInput = ({ value, onChange }) => {
   return (
-    <Box mx="auto" width="10%">
+    <Box mx="auto" width={{ base: "80%", md: "40%", lg: "20%" }}>
+      {" "}
+  
       <Input
         placeholder="Search events"
         value={value}
